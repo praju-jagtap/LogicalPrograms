a4591611e2f203f5ec45804c8dd02945ddc87152
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-08-27 19:26:44 +0530

    [Prajakta] Add: Display Fibonacci Series of a Number

---
+++ LogicalProgram/FibonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class FibonacciSeries
+    {
+        int firstNum = 0, secondNum = 1, thirdNum = 0, i;
+        public void Fibonacci()
+        {
+            Console.Write("\n");
+            Console.Write("To Print Febonacci Series:\n");
+            Console.Write("----------------------------------------------\n");
+            Console.Write("\n");
+            Console.WriteLine("Enter the Number to Print FibonacciSeries : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Febonacci Series is : \n" + firstNum + "\n" + secondNum);
+            while (num >= 2)
+            {
+                thirdNum = firstNum + secondNum;
+                Console.WriteLine(thirdNum + "");
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                num--;
+            }
+        }
+    }
+}
--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Logical Program");
+            Console.WriteLine("1.FibonacciSeries\n");
+            Console.WriteLine("Please Enter Your Choice : ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibonacciSeries series = new FibonacciSeries();
+                    series.Fibonacci();
+                    break;
+                
+                default:
+                    break;
+            }
         }
     }
 }
