e8c0b9789e71ff645bb333117ca5b14655453c48
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-08-28 11:17:07 +0530

    [Prajakta] Add: Check Perfect No Or Not

---
+++ LogicalProgram/PerfectNo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class PerfectNo
+    {
+
+        int numCheck = 1, sum = 0,i ;
+        public void Perfect()
+        {
+            Console.Write("\n");
+            Console.Write("To Print Perfect no:\n");
+            Console.Write("----------------------------------------------\n");
+            Console.Write("\n");
+            Console.WriteLine("Enter The Number To Check Either its Perfect No or Not:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= num/2; i++)
+            {
+                numCheck = num % i; 
+                if (numCheck == 0)
+                {
+                    sum += i;
+                }
+            }
+            if (sum == num)
+            {
+                Console.WriteLine("Number is Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine("Number is Not a Perfect Number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.FibonacciSeries\n");
+            Console.WriteLine("1.FibonacciSeries \n2.Perfect Number");
             Console.WriteLine("Please Enter Your Choice : ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -12,6 +12,10 @@
                 case 1:
                     FibonacciSeries series = new FibonacciSeries();
                     series.Fibonacci();
+                    break;
+                case 2:
+                    PerfectNo perfectNo = new PerfectNo();
+                    perfectNo.Perfect();
                     break;
                 
                 default:
